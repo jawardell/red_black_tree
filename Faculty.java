5b92334c1248b399d158b3ca96805c4ba49f090b
Austin Hufstetler <austin_hufstetler@live.com>
2017-04-01 17:14:54 -0400

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -9,4 +9,25 @@ public class Faculty {
 	public Faculty(){
 		
 	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public long getID(){
+		return id;
+	}
+	
+	public long getCell(){
+		return cell;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public String toString(){
+		String s;
+		return s;
+	}
 }
