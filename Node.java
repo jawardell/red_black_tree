fed8ce16d765e756fc3c0a1b81e10f8f9cc9f800
jawardell <jawardell@valdosta.edu>
2017-04-01 16:19:34 -0400

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,21 @@
+package essentials;
+
+import schooldatabase.Student;
+import schooldatabase.Faculty;
+import whocalled.PhoneNumber;
+
+public class Node {
+	
+	public Node(String phoneNumber, PhoneNumber p){
+		
+	}
+	
+	public Node(String lastName, Student s){
+		
+	}
+	
+	public Node(String lastName, Faculty f){
+		
+	}
+	
+}
