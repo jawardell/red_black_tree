d36025ebee61adc33e73439333d4e84d8ce9249c
jawardell <jawardell@valdosta.edu>
2017-04-02 00:14:50 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,32 +6,32 @@ public class Student {
 	String classification;
 	String email;
 	
-	public Student(String name, long id, String classification, String email){
+	public Student(String name, long id, String classification, String email) {
 		this.name = name;
 		this.id = id;
 		this.classification = classification;
 		this.email = email;
 	}
 	
-	public String getName(){
+	public String getName() {
 		return name;
 	}
 	
-	public long getID(){
+	public long getID() {
 		return id;
 	}
 	
-	public String getClassification(){
+	public String getClassification() {
 		return classification;
 	}
 	
-	public String getEmail(){
+	public String getEmail() {
 		return email;
 	}
 	
 	//do we want setters? 
 	
-	public String toString(){
+	public String toString() {
 		return "\nStudent\nName: " + name + " ID: " + id + " Classification: " + classification + "Email: " + email;
 	}
 }
