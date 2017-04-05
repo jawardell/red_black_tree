865a7e344aa4f274d32dfaad631b34c1dcebdab4
Austin Hufstetler <austin_hufstetler@live.com>
2017-04-04 22:04:30 -0400

    Rewrote
    took from codebytes

--- Tester.java
+++ Tester.java
@@ -2,9 +2,9 @@ package essentials;
 
 public class Tester {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public static void main(String[] args) {
+	RedBlackTree rbt = new RedBlackTree();
+	rbt.consoleUI();
+    }
 
 }
