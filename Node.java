8a144c7fb7a2584e8803c776d00bef3a7ce2c62b
Austin Hufstetler <austin_hufstetler@live.com>
2017-04-04 22:02:49 -0400

    Rewrote
    took from codebytes

--- Node.java
+++ Node.java
@@ -1,59 +1,12 @@
-package essentials;
-
-import schooldatabase.Student;
-import schooldatabase.Faculty;
-import whocalled.PhoneNumber;
+//http://www.codebytes.in/2014/10/red-black-tree-java-implementation.html
+//tooken from codebytes
 
 public class Node {
-	private String key;//We could make this a generic type. I chose String because we will be sorting on name (maybe).
-	private Node left, right;
-	public String[] data;
-	private Boolean isRed;
-	public Node(String key, String[] data) {
-		Node(key, data, null, null);
-	}
-	
-	public Node(String key, String[] data, Node left, Node right){
-		this.left = left;
-		this.right = right;
-		isRed = false; //inserted node will be black first so initialize as black first
-		this.key = key;
-		this.data = data;
-		
-	}
-	public Node getLeft() {
-		if(left != null) {
-			return left;
-		}
-		else {
-			return null;
-		}
-	}
-	
-	public Node getRight() {
-		if(right != null) {
-			return right;
-		}
-		else {
-			return null;
-		}
-	}
-	
-	public Node getRoot() {
-		if(root != null) {
-			return root;
-		}
-		else {
-			return null;
-		}
-	}
-	
-	public boolean isRed() {
-		return isRed;
-	}
-	
-	public String getData() {
-		return data;
-	}
-	
+
+	int key = -1, color = BLACK;
+	Node left = nil, right = nil, parent = nil;
+
+	Node(int key) {
+	    this.key = key;
+	} 
 }
