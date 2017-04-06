cbf481b6c7c67c798674bd9d49d788a43cd354fc
Austin Hufstetler <austin_hufstetler@live.com>
2017-04-05 22:55:01 -0400

    Added Constructors
    Added constructors to work with our data

--- Node.java
+++ Node.java
@@ -7,12 +7,16 @@ class RedBlackNode
     RedBlackNode left, right;
     int element;
     int color;
+    Student s;
+    Faculty f;
+    PhoneNumber p;
 
     /* Constructor */
     public RedBlackNode(int theElement)
     {
         this( theElement, null, null );
     }
+    
     /* Constructor */
     public RedBlackNode(int theElement, RedBlackNode lt, RedBlackNode rt)
     {
@@ -21,4 +25,51 @@ class RedBlackNode
         element = theElement;
         color = 1;
     }
+    
+    /* Constructor for Student*/
+    public RedBlackNode(Student s)
+    {
+        this( s, null, null );
+    }
+    
+    /* Constructor */
+    public RedBlackNode(Student s, RedBlackNode lt, RedBlackNode rt)
+    {
+        left = lt;
+        right = rt;
+        this.s = s;
+        color = 1;
+    }
+    
+     /* Constructor for Faculty*/
+    public RedBlackNode(Faculty f)
+    {
+        this( f, null, null );
+    }
+    
+    /* Constructor */
+    public RedBlackNode(Faculty f, RedBlackNode lt, RedBlackNode rt)
+    {
+        left = lt;
+        right = rt;
+        this.f = f;
+        color = 1;
+    }
+
+     /* Constructor for Phone Number */
+    public RedBlackNode(PhoneNumber p)
+    {
+        this( p, null, null );
+    }
+    
+    /* Constructor */
+    public RedBlackNode(PhoneNumber p, RedBlackNode lt, RedBlackNode rt)
+    {
+        left = lt;
+        right = rt;
+        this.p = p;
+        color = 1;
+    }
+    
+
 }
