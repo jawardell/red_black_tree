04bcfed5fa508abf92fb7412321d27a3fba87cbb
jawardell <jawardell@valdosta.edu>
2017-04-15 15:27:44 -0400

    user promt to begin
    At this point, the progra requires some dummy input to enter the while loop. We can fix this with a do while loop, boolean flags, or many other ways. When we decide to fix this problem, we need to be careful because there are many if, else, and other conditional statements contained in this big while loop; thus making alterations could mess things up. So for now, we prompt the user to enter some dummy input in a nice way "type begin to search". Of course, the input does not have to be "begin", it just has to be anything other than "exit". When the dummy input is provided and is not "exit" the program continues. Again, this problem can be corrected in a number of ways and doing so would have to be done carefully.

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ class Main {
 			names.put(phoneNum, name);// kd saves the day w/ hm
 		}
 		scan.close();
-
+		// System.out.print("Type \"begin\" to search.\n\n");
 		Scanner input = new Scanner(System.in);
 		/*
 		 * The while loop is the concept we should chase. We want to keep the
@@ -27,8 +27,11 @@ class Main {
 		 * search P.S. The braces for the while loop have been placed in their
 		 * respective location
 		 */
-		while (input.next() != "exit") {
-			System.out.print("enter country code: ");
+
+		System.out.print("To search type \"begin\". To quit, type \"exit\". \n");
+		while (!input.next().equals("exit")) {
+
+			System.out.print("\n\nenter country code: ");
 			String countryCodeStr = input.next();
 			System.out.print("\nenter rest of phone number: ");
 			String restOfNumStr = input.next();
@@ -38,26 +41,23 @@ class Main {
 
 			boolean isThere = tree.search(goodPhoneNumber);
 
+			switch (countryCodeInt) {
+			case (1):
+				countryName = "United States/Canada";
+				break;
+			case (91):
+				countryName = "India";
+				break;
+			case (52):
+				countryName = "Mexico";
+				break;
+			case (61):
+				countryName = "Australia";
+				break;
+			default:
+				countryName = "unlisted";
+			}
 
-
-
-    switch (countryCodeInt) {
-				case (1):
-					countryName = "United States/Canada";
-					break;
-				case (91):
-					countryName = "India";
-					break;
-				case (52):
-					countryName = "Mexico";
-					break;
-				case (61):
-					countryName = "Australia";
-					break;
-				default:
-					countryName = "unlisted";
-				}
-            
 			if (isThere) {
 
 				finalToString += "\n\nSearch Succesful!\nCountry: " + countryName + "\n";
@@ -68,17 +68,22 @@ class Main {
 
 				finalToString += "Phone Number: " + (countryCodeStr + restOfNumStr) + "\n";
 
-            System.out.println(finalToString);
-        
-            
+				System.out.println(finalToString);
+
+				System.out.print("To begin new search, type \"begin\". Type \"exit\" to quit.\n");
+
 			} else {
 				// changed to a print statement because it now serves as a
 				// prompt instead of
 				// a final toString
-            
-				//System.out.print("The search was inconslusive.\nAdd phone number to our database? (Y/N)");// ask yes or no
-            System.out.print("The search was inconslusive, the call originated from " + countryName + "\nAdd phone number to our database? (Y/N)");// ask yes or no
-																											
+
+				// System.out.print("The search was inconslusive.\nAdd phone
+				// number to our database? (Y/N)");// ask yes or no
+				System.out.print("The search was inconslusive, the call originated from " + countryName
+						+ "\nAdd phone number to our database? (Y/N)");// ask
+																		// yes
+																		// or no
+
 				String addNum = input.next();
 				if (addNum.equalsIgnoreCase("y")) {
 					// if yes, add phone
