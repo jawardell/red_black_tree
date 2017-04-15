0b8938f7d1096d757ae31e908e2454d7692091dd
KD <young.adkins@gmail.com>
2017-04-15 13:50:35 -0400

    Can search by name and ID

--- Main2.java
+++ Main2.java
@@ -6,7 +6,7 @@ class Main2 {
 		String countryName = "";
 		File file = new File("SchoolDB.txt");
 		Scanner scan = new Scanner(file);
-		HashMap<Long, String> names = new HashMap<>();
+		HashMap<String, Long> names = new HashMap<>();
 		RBTree tree = new RBTree(0);
 		String finalToString = "";
 		while (scan.hasNext()) {
@@ -18,11 +18,15 @@ class Main2 {
          String CorP = scan.next(); //classification or phone number
          scan.nextLine(); //to avoid the problem with different # of arguments, doesnt work though lol
 			tree.insert(id);
-			names.put(id, lastName);// kd saves the day w/ hm
+			names.put(firstName + " " + lastName, id);// kd saves the day w/ hm
 		}
 		scan.close();
       
 		Scanner input = new Scanner(System.in);
+      long identify = -1;
+      boolean isThere = false;
+      long duration = 0;
+      String possibles = "";
 		/*
 		 * The while loop is the concept we should chase. We want to keep the
 		 * command line going until a condition is met which means we need a
@@ -31,15 +35,32 @@ class Main2 {
 		 * search P.S. The braces for the while loop have been placed in their
 		 * respective location
 		 */
-		while (input.next() != "exit") {
-			System.out.print("Enter student ID:");
-			long identify = input.nextLong();
+      System.out.print("Would you like to search by name or ID? ");
+      String choice = input.next();
+		while (choice != "exit") {
+         if(choice.equalsIgnoreCase("ID")){
+			   System.out.print("Enter student ID:");
+			   identify = input.nextLong();
 
-			boolean isThere = tree.search(identify);
-
+			   isThere = tree.search(identify);
+         }
+         if(choice.equalsIgnoreCase("Name")){
+            System.out.println("Enter first or last name: ");
+            String key = input.next();
+            
+            for(Map.Entry<String,Long> match: names.entrySet()){
+               long t1 = System.nanoTime();
+               if(match.getKey().contains(key)){
+                  possibles += match.getKey() + "\n";
+                  duration = System.nanoTime() - t1;
+               }
+            }
+            //System.out.println("Search Complete. There were multiple occurences of the name" +
+            //                   " you searched
+         }
 			if (isThere) {
 
-            System.out.println("it's there! " + identify);
+            System.out.println("it's there! " + identify + "***" + ((double)duration/1000000000));
 				}
 
 				/*finalToString += "\n\nSearch Succesful!\nCountry: " + countryName + "\n";
@@ -54,7 +75,8 @@ class Main2 {
 				// changed to a print statement because it now serves as a
 				// prompt instead of
 				// a final toString
-				System.out.print("The search was inconslusive.\nAdd phone number to our database? (Y/N)");// ask yes or no
+				System.out.print("The search was inconslusive.The student you searched for doesn't exist" +
+                              " in our database;");// ask yes or no
 																											
 				String addNum = input.next();
 				if (addNum.equals("y")) {
