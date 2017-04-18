319f5f39c6ebc412966e878db83b8e44a850e3aa
jawardell <jawardell@valdosta.edu>
2017-04-18 17:56:31 -0400

    Update Main.java

--- jo/source/whocalled/Main.java
+++ jo/source/whocalled/Main.java
@@ -19,17 +19,7 @@ class Main {
 			names.put(phoneNum, name);// kd saves the day w/ hm
 		}
 		scan.close();
-		// System.out.print("Type \"begin\" to search.\n\n");
 		Scanner input = new Scanner(System.in);
-		/*
-		 * The while loop is the concept we should chase. We want to keep the
-		 * command line going until a condition is met which means we need a
-		 * loop. The while loop will bring the user back to the top if they
-		 * specify in the bottom if-statement, that they want to do another
-		 * search P.S. The braces for the while loop have been placed in their
-		 * respective location
-		 */
-
 		System.out.print("To search type \"begin\". To quit, type \"exit\". \n");
 		while (!input.next().equals("exit")) {
 
@@ -44,25 +34,25 @@ class Main {
 			boolean isThere = tree.search(goodPhoneNumber);
 
 			switch (countryCodeInt) {
-			case (1):
-				countryName = "United States/Canada";
-				break;
-			case (91):
-				countryName = "India";
-				break;
-			case (52):
-				countryName = "Mexico";
-				break;
-			case (61):
-				countryName = "Australia";
-				break;
-			default:
-				countryName = "unlisted";
+				case (1):
+					countryName = "United States/Canada";
+					break;
+				case (91):
+					countryName = "India";
+					break;
+				case (52):
+					countryName = "Mexico";
+					break;
+				case (61):
+					countryName = "Australia";
+					break;
+				default:
+					countryName = "unlisted";
 			}
 
 			if (isThere) {
 
-				finalToString += "\n\nSearch Succesful!\nCountry: " + countryName + "\n";
+				finalToString += "\n\nSearch Successful!\nCountry: " + countryName + "\n";
 
 				String callerName = names.get(goodPhoneNumber);
 
@@ -75,40 +65,27 @@ class Main {
 				System.out.print("To begin new search, type \"begin\". Type \"exit\" to quit.\n");
 
 			} else {
-				// changed to a print statement because it now serves as a
-				// prompt instead of
-				// a final toString
 
-				// System.out.print("The search was inconslusive.\nAdd phone
-				// number to our database? (Y/N)");// ask yes or no
-				System.out.print("The search was inconslusive, the call originated from " + countryName
-						+ "\nAdd phone number to our database? (Y/N)");// ask
-																		// yes
-																		// or no
-
+				System.out.print("The search was inconclusive, the call originated from " + countryName
+						+ "\nAdd phone number to our database? (Y/N)");
 				String addNum = input.next();
 				if (addNum.equalsIgnoreCase("y")) {
-					// if yes, add phone
 					tree.insert(goodPhoneNumber);
 					System.out.print("The phone number has been added successfully. Thank you for your assistance."
 							+ " Have a great day!");
 				} else {
-					// if no, ask to search/add again
 					System.out.println("Would you like to search another number?(Y/N)");
 					String answer = input.next();
 					if (answer.equalsIgnoreCase("y")) {
-						// leads user to beginning of while loop
 						continue;
 					} else {
 						finalToString += "GoodBye! \t\t Type 'exit' to end program";
-						break;// hops out of outer-most if-statement
-						// Note: while loop is still running
+						break;
 					}
 
 				}
 				System.out.println(finalToString);
-
-			} // end of while
+			} 
 		}
 	}
 }
