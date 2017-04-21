1bb6b72d36e3672d8f90d45eebfbf7c2bb34f892
Austin Hufstetler <austin_hufstetler@live.com>
2017-04-21 13:38:04 -0400

    updated files with some corrections
    also, added header for school databse

---
+++ Main2Updated.java
@@ -0,0 +1,124 @@
+import java.util.*;
+import java.io.*;
+import java.io.FileNotFoundException;
+public class Main2Updated {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("SchoolDB.txt");
+		TreeMap<String, Person> names = new TreeMap<>();
+		TreeMap<Long, Person> ids = new TreeMap<>();
+		RBTree tree = new RBTree(0);
+		Scanner scanner = new Scanner(file), input = new Scanner(System.in);
+		String searchByName = "", possible, choice = "";
+		boolean found = false;
+		long searchById = 0;
+		int count = 0;
+      boolean exit = true;
+
+
+		while(scanner.hasNext()) {
+			String role = scanner.next();
+			long id = scanner.nextLong();
+			String firstName = scanner.next();
+			String lastName = scanner.next();
+			String email = scanner.next();
+			String corp = scanner.next();
+			Person person = new Person(role, id, firstName + " " + lastName, corp, email);
+			names.put(person.getName(), person);
+			ids.put(person.getID(), person);
+			tree.insert(person.getID());
+		}
+		scanner.close();
+      
+      System.out.println();
+      System.out.println("**************************************************************");
+      System.out.println("**        Welcome to Sierra University People Search        **");
+      System.out.println("**                                                          **");
+      System.out.println("**    For security reasons,Our database is limited to use   **" );
+      System.out.println("**               by students and faculty                    **");
+      System.out.println("**                                                          **");
+      System.out.println("**************************************************************");
+      System.out.println();
+      System.out.println();
+      System.out.println("If you see any problems with our database, please call us at 908-674-1124");
+      System.out.println();
+      System.out.println();
+      
+      
+		while(exit) {
+			possible = "";
+			String key = "";
+			//System.out.print("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nNew Search\n\n");
+			System.out.print("Search by name or ID:\n");
+			choice = input.next();
+			if(choice.equalsIgnoreCase("Name")) {
+				System.out.print("Enter \"1\" for first and last name format.");
+				System.out.print("\nEnter \"2\" for single name format.\n");
+				int casey = input.nextInt();
+				switch(casey) {
+					case 1 :
+						System.out.print("Enter first and last name:\n");
+						key = input.next() + " " + input.next();
+						break;
+					case 2:
+						System.out.print("Enter single name:\n");
+						key = input.next();
+						break;
+				}
+
+				for(String name1 : names.keySet()) {
+					if(name1.contains(key)) {
+						count++;
+						possible += names.get(name1).getName() + "\n";
+					}
+				}
+
+
+				if(count > 1) {
+					System.out.print("There were multiple occurrences:\n");
+					System.out.print(possible);
+					System.out.print("\nType the first and last name of one you want:\n");
+					searchByName = input.next();
+					searchByName += " " + input.next();
+					found = true;
+				} else if (count == 1) {
+					searchByName = possible.substring(0, possible.length()-1);
+					found = true;
+				} else {
+					System.out.print("There are 0 occurrences");
+				}
+
+				if(found) {
+					Person person = names.get(searchByName);
+					System.out.print(person.toString());
+				}
+
+			}
+
+
+
+
+			if(choice.equalsIgnoreCase("ID")) {
+				System.out.print("Enter ID:\n");
+				searchById = input.nextLong();
+				if(tree.search(searchById)) {//this tiny line searches thru 2000+ ids @ lightning speed
+					Person person = ids.get(searchById);
+					System.out.print("\nSearch Successful!\n" + person.toString());
+				} else {
+					System.out.print("\nSearch Failed.");
+				}
+			}
+         
+         System.out.println();
+         System.out.println("\nWould you like to search again? [Y,N]");
+         Scanner scan2 = new Scanner(System.in);
+         String answer = scan2.next();
+         if (answer.equals("Y")){
+         
+         }
+         else
+            exit = false;
+		}
+
+
+	}
+}
---
+++ MainUpdated.java
@@ -0,0 +1,83 @@
+//package whocalled;
+import java.util.*;
+import java.io.*;
+class MainUpdated {
+	public static void main(String[] args) throws Exception {
+		String countryName = "";
+		File file = new File("data.txt");
+		Scanner scan = new Scanner(file);
+		HashMap<Long, String> names = new HashMap<>();
+		RBTree tree = new RBTree(0);
+		String finalToString = "", countryCodeStr = "";
+      boolean exit = true;
+		while (scan.hasNext()) {
+			String country = scan.next();
+			long phoneNum = scan.nextLong();
+			String name = scan.next();
+			tree.insert(phoneNum);
+			names.put(phoneNum, name);
+		}
+		scan.close();
+		Scanner input = new Scanner(System.in);
+		System.out.print("\n\n\"exit\" ends the program.\n");
+      
+		while (exit) {
+         System.out.println("enter country code: ");
+			countryCodeStr = input.next();
+			System.out.print("\nenter rest of phone number: ");
+			String restOfNumStr = input.next();
+
+			int countryCodeInt = Integer.parseInt(countryCodeStr);
+			long goodPhoneNumber = Long.parseLong((countryCodeStr + restOfNumStr));
+
+			boolean isThere = tree.search(goodPhoneNumber);
+
+			switch (countryCodeInt) {
+				case (1):
+					countryName = "United States/Canada";
+					break;
+				case (91):
+					countryName = "India";
+					break;
+				case (52):
+					countryName = "Mexico";
+					break;
+				case (61):
+					countryName = "Australia";
+					break;
+				default:
+					countryName = "unlisted";
+			}
+
+			if (isThere) {
+				finalToString += "\n\nSearch Successful!\nCountry: " + countryName + "\n";
+				String callerName = names.get(goodPhoneNumber);
+				finalToString += "Caller Name: " + callerName + "\n";
+				finalToString += "Phone Number: " + (countryCodeStr + restOfNumStr) + "\n";
+				System.out.println(finalToString);
+				System.out.println("Search again?\n");
+				String answer = input.next();
+				if (answer.equalsIgnoreCase("y")) {
+					exit = true;
+				} else {
+					exit = false;
+
+				}
+			} else {
+
+				System.out.print("The search was inconclusive, the call originated from " + countryName
+						+ "\nSearch again? (Y/N)");
+				String answer = input.next();
+				if (answer.equalsIgnoreCase("y")) {
+					exit = true;
+				} else {
+					exit = false;
+
+				}
+				System.out.println(finalToString);
+            
+            
+			} 
+		}
+	}
+}
