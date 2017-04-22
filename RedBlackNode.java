f014d8a863328ee4c483064654f5a5d359793a27
jawardell <jawardell@valdosta.edu>
2017-04-22 17:31:17 -0400

    Update RedBlackNode.java

--- RedBlackNode.java
+++ RedBlackNode.java
@@ -1,27 +1,25 @@
 //http://www.sanfoundry.com/java-program-implement-red-black-tree/
-//tooken from codebytes
+//taken from codebytes
 
 /* Class Node */
-class RedBlackNode
-{
-    RedBlackNode left, right;
-    long element;
-    int color;
+class RedBlackNode {
 
-    /* Constructor */
-    public RedBlackNode(long theElement)
-    {
-        this( theElement, null, null );
-    }
-    
-    /* Constructor */
-    public RedBlackNode(long theElement, RedBlackNode lt, RedBlackNode rt)
-    {
-        left = lt;
-        right = rt;
-        element = theElement;
-        color = 1;
-    }
-    
+	public RedBlackNode(long theElement) {
+		this(theElement, null, null);
+	}
+
+	RedBlackNode left, right;
+	long element;
+	int color;
+
+	/* Constructor */
+
+	/* Constructor */
+	public RedBlackNode(long theElement, RedBlackNode lt, RedBlackNode rt) {
+		left = lt;
+		right = rt;
+		element = theElement;
+		color = 1;
+	}
 
 }
