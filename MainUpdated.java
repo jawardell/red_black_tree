27dc5d012b899a4a53c04c82c0a65d45da8a0a37
Austin Hufstetler <austin_hufstetler@live.com>
2017-04-22 16:49:18 -0400

    Update MainUpdated.java

--- MainUpdated.java
+++ MainUpdated.java
@@ -1,6 +1,6 @@
+//package whocalled;
 import java.util.*;
 import java.io.*;
-
 class MainUpdated {
 	public static void main(String[] args) throws Exception {
 		String countryName = "";
@@ -9,7 +9,7 @@ class MainUpdated {
 		HashMap<Long, String> names = new HashMap<>();
 		RBTree tree = new RBTree(0);
 		String finalToString = "", countryCodeStr = "";
-		boolean exit = true;
+      boolean exit = true;
 		while (scan.hasNext()) {
 			String country = scan.next();
 			long phoneNum = scan.nextLong();
@@ -19,16 +19,16 @@ class MainUpdated {
 		}
 		scan.close();
 		Scanner input = new Scanner(System.in);
-		System.out.println("\n**************************************************************"
-				+ "\n**        Welcome to WHOCALLED phone lookup service         **"
-				+ "\n**                                                          **"
-				+ "\n**   Have you received a call from a strange number lately? **"
-				+ "\n**               We're here to help                         **"
-				+ "\n**                                                          **"
-				+ "\n**************************************************************\n\n");
-
+      System.out.println("\n**************************************************************"
+      			+ "\n**        Welcome to WHOCALLED phone lookup service         **" 
+	      		+ "\n**                                                          **"
+      			+ "\n**   Have you received a call from a strange number lately? **"
+      			+ "\n**               We're here to help                         **"
+      			+ "\n**                                                          **"
+      			+ "\n**************************************************************\n\n");
+      
 		while (exit) {
-			System.out.println("enter country code: ");
+         System.out.println("enter country code: ");
 			countryCodeStr = input.next();
 			System.out.print("\nenter rest of phone number: ");
 			String restOfNumStr = input.next();
@@ -39,20 +39,62 @@ class MainUpdated {
 			boolean isThere = tree.search(goodPhoneNumber);
 
 			switch (countryCodeInt) {
-			case (1):
-				countryName = "United States/Canada";
-				break;
-			case (91):
-				countryName = "India";
-				break;
-			case (52):
-				countryName = "Mexico";
-				break;
-			case (61):
-				countryName = "Australia";
-				break;
-			default:
-				countryName = "unlisted";
+				case (1):
+					countryName = "United States/Canada";
+					break;
+				case (91):
+					countryName = "India";
+					break;
+				case (52):
+					countryName = "Mexico";
+					break;
+				case (61):
+					countryName = "Australia";
+					break;
+				case (501):
+					countryName = "Belize";
+					break;
+				case (975):
+					countryName = "Bhutan";
+					break;
+				case (90):
+					countryName = "Turkey";
+					break;
+				case (256):
+					countryName = "Uganda";
+					break;
+				case (260):
+					countryName = "Zambia";
+					break;
+				case (998):
+					countryName = "Uzbekistan";
+					break;
+				case (66):
+					countryName = "Thailand";
+					break;
+				case (20):
+					countryName = "Egypt";
+					break;
+				case (672):
+					countryName = "Antarctica";
+					break;
+				case (56):
+					countryName = "Chile";
+					break;
+				case (57):
+					countryName = "Colombia";
+					break;
+				case (30):
+					countryName = "Greece";
+					break;
+				case (31):
+					countryName = "Netherlands";
+					break;
+				case (381):
+					countryName = "Serbia";
+               break;
+				default:
+					countryName = "unlisted";
 			}
 
 			if (isThere) {
@@ -61,7 +103,7 @@ class MainUpdated {
 				finalToString += "Caller Name: " + callerName + "\n";
 				finalToString += "Phone Number: " + (countryCodeStr + restOfNumStr) + "\n";
 				System.out.println(finalToString);
-				System.out.println("Search again?(Y/N)\n");
+				System.out.println("Search again?\n");
 				String answer = input.next();
 				if (answer.equalsIgnoreCase("y")) {
 					exit = true;
@@ -81,8 +123,9 @@ class MainUpdated {
 
 				}
 				System.out.println(finalToString);
-
-			}
+            
+            
+			} 
 		}
 	}
 }
